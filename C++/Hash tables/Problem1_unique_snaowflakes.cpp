b1d300d3f094d15643eb5f3d6b5b4cef0d687ac7
JeganT143 <engjegant@gmail.com>
2025-10-31 13:12:40 +0530

    hashing simplified problem

---
+++ C++/Hash tables/Problem1_unique_snaowflakes.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+//=====================================================================================================================================
+// void identify_identical(int values[], int n) {
+//     for (int i = 0; i < n ; i++) {
+//         for (int j = i+1 ; j < n ; j++) {
+//             if (values[i] == values[j]) {
+//                 cout << "Twin integers found for i = " << i << " and j = " << j << " also arr[i] = arr[j] = " << values[i]  << endl;
+//                 return;
+//             }
+//         }
+//     }
+//
+//     cout << "Twins Not Found " << endl;
+//
+// }
+//
+// int main() {
+//
+//     int n = 5 ;
+//     int arr[5] = {10,15,20,15,30};s
+//     identify_identical(arr,5);
+//
+// }
+// *************************************************************************************************************************************
